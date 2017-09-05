3919733f5d95926f63b2c59f0b78283a0a4a162e
Dzianis Sipakou <d.sipakou@gmail.com>
2017-09-05 16:34:33 +0300

    Rectructure

--- src/containers/board/Board.js
+++ src/containers/board/Board.js
@@ -1,8 +1,13 @@
 import React, {Component} from 'react';
+import Card from '../../components/card/Card';
 
 class Board extends Component {
   render() {
-    return <div>Board here</div>;
+    return (
+    <div>Board here
+      <Card/>
+    </div>
+    );
   }
 }
 
