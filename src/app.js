efd2276911c58c146d0a6415b64bb750860069d4
Dzianis Sipakou <d.sipakou@gmail.com>
2017-09-06 16:33:02 +0300

    Add basic styling

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM, {render} from 'react-dom';
 import Board from './containers/board/Board';
+import './style/base.scss';
 
 render(<Board/>, document.getElementById('app'));
