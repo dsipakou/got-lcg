b4ed61790d01ba2c19d9f979d82f1ff0429c8dad
Dzianis Sipakou <d.sipakou@gmail.com>
2017-09-12 15:10:56 +0300

    Rename cards.js to hand.js

--- src/redux/hand.js
+++ src/redux/hand.js
@@ -1,13 +1,13 @@
 import { createStore } from 'redux';
 
 const initialState = [
-  {id: 23, revealed: true, name: 'New card'},
-  {id: 543, revealed: false, name: 'Old card'},
-  {id: 54, revealed: true, name: 'Funny card'},
-  {id: 44, revealed: true, name: 'One more card'}
+  {id: 23, revealed: true, name: 'Lannister', type: 'CHARACTER'},
+  {id: 543, revealed: false, name: 'Stark', type: 'CHARACTER'},
+  {id: 54, revealed: true, name: 'Winterfell', type: 'LOCATION'},
+  {id: 44, revealed: true, name: 'Wall', type: 'LOCATION'}
 ]
 
-const deckReducer = (state = initialState, action) => {
+function deckReducer(state = initialState, action) {
   switch (action.type) {
     case 'PLACE_CARD':
       const index = action.index;
@@ -24,4 +24,4 @@ const deckReducer = (state = initialState, action) => {
   }
 }
 
-const store = createStore(deckReducer)
+export default deckReducer;
