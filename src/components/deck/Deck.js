2aa655caa5299d345a6555e4e9773298acae53ea
flyermc <mc.flyer@gmail.com>
2017-09-15 08:33:19 +0300

    Finish draw card POC

--- src/components/deck/Deck.js
+++ src/components/deck/Deck.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 import './Deck.scss';
 
-const Deck = (onDeckClick) => {
-  return (
-    <div className='deck-inner' onClick={onDeckClick}>Deck here</div>
-  )
+class Deck extends Component {
+  render() {
+  const { onDeckClick } = this.props
+    return (
+      <div className='deck-inner' onClick={onDeckClick}>Deck here</div>
+    )
+  }
 }
 
 Deck.propTypes = {
--- src/components/hand/hand.js
+++ src/components/hand/hand.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Card from '../card/card';
 import { connect } from 'react-redux';
-import { playCard } from '../../redux/actions/card';
 import { bindActionCreators } from 'redux';
 import './hand.scss';
 
@@ -15,14 +14,14 @@ class Hand extends Component {
   componentDidMount() {
     console.log(this.props);
   }
-  
+
   render() {
     const { cards, playCard } = this.props;
     console.log(cards)
     return (
       <div className='hand'>
         { cards.map((card, index) => (
-          <Card {...card} index={index} key={card.id} revealed={card.revealed} name={card.name} onCardClick={playCard}/>
+          <Card {...card} index={index} key={card.id} revealed={card.revealed} name={card.name}/>
         )) }
       </div>
     );
@@ -31,7 +30,6 @@ class Hand extends Component {
 
 Hand.propTypes = {
   cards: PropTypes.array,
-  playCard: PropTypes.func.isRequired,
 }
 
 export default Hand;
