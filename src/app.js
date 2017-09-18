44082c50ff59ae09887ebd2c0f52f436a2b9d8a6
Dzianis Sipakou <d.sipakou@gmail.com>
2017-09-18 15:16:18 +0300

    Add thunk as middleware

--- src/app.js
+++ src/app.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM, {render} from 'react-dom';
 import Board from './containers/board/Board';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import player from './redux/reducers/player';
 import './style/base.scss';
 
-const store = createStore(player, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(
+	player,
+	compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  ),
+);
 
 render(
   <Provider store={store}>
