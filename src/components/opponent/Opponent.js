4f112b5c4ff644a14171d27c098d1e49114ae079
flyermc <mc.flyer@gmail.com>
2017-09-29 08:46:38 +0300

    Add opponent component

---
+++ src/components/opponent/Opponent.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Location from '../location/location';
+import Character from '../character/character';
+import './Opponent.scss';
+
+const Opponent = ({characters, locations, characterActions, locationActions}) => {
+  return (
+    <div className='opponent-inner'>
+      <div className='play-zone'>
+        <div className='permanent-cards'>
+           asdfasfasfsa
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Opponent;
