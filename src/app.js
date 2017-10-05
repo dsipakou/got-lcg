ef832a32eaae1a8b3cb7c2be72043e1676fd9780
Dzianis Sipakou <d.sipakou@gmail.com>
2017-10-05 18:57:00 +0300

    Wrap routes by Layout

--- src/app.js
+++ src/app.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM, {render} from 'react-dom';
 import Board from './containers/board/Board';
 import Lobby from './components/lobby/Lobby';
+import Layout from './components/Layout';
 import { Provider } from 'react-redux';
-import { browserHistory, BrowserRouter, Route, Switch } from 'react-router-dom';
+import { browserHistory, BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import player from './redux/reducers/player';
@@ -30,10 +31,12 @@ const store = createStore(
 render(
   <Provider store={store}>
 		<BrowserRouter>
-			<Switch>
-				<Route path="/" component={connectSocket(Board)} />
-				<Route path="/lobby" component={Lobby} />
-			</Switch>
+			<Layout>
+				<Switch>
+					<Route exact path="/" component={connectSocket(Board)} />
+					<Route path="/lobby" component={Lobby} />
+				</Switch>
+			</Layout>
 		</BrowserRouter>
   </Provider>,
   document.getElementById('app')
