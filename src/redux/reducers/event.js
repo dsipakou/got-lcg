7833bd07a6cc0750e47e958e234e1ec19331d31e
Dzianis Sipakou <d.sipakou@gmail.com>
2017-10-09 13:23:33 +0300

    Update cardlocation for event

--- src/redux/reducers/event.js
+++ src/redux/reducers/event.js
@@ -1,9 +1,15 @@
 import { ADD_EVENT } from '../actions/event';
+import update from 'react-addons-update';
 
 function eventReducer(state = {}, action) {
   switch (action.type) {
     case ADD_EVENT:
-      return action.payload;
+      const card = update(action, {
+        payload: {
+          cardlocation: {$set: 'EVENT'}
+        }
+      }).payload
+      return card;
     default:
       return state;
   }
