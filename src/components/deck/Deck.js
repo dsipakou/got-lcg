37cde6cc6e029744e72d26be4146d1ede310e795
Dzianis Sipakou <d.sipakou@gmail.com>
2017-10-09 16:21:20 +0300

    Add empty-deck className

--- src/components/deck/Deck.js
+++ src/components/deck/Deck.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
 import './Deck.scss';
 
 const Deck = ({ deck, action }) => {
   const doAction = () => {
     action(deck.length)
   }
+
+  let deckClass = classNames({
+    'deck-inner': true,
+    'empty-deck': deck.length == 0
+  });
+
   return (
-    <div className='deck-inner' onClick={doAction}><span>Click to draw</span>
+    <div className={deckClass} onClick={doAction}>
       <div className='deck-spinner'></div>
     </div>
   )
