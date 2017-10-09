649b4f32ca278b5a7068e9046ffe454ce6dd6bde
Dzianis Sipakou <d.sipakou@gmail.com>
2017-10-09 16:22:21 +0300

    Refactor discardPile

--- src/components/discardPile/DiscardPile.js
+++ src/components/discardPile/DiscardPile.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Card from '../../components/card/Card';
+import Deck from '../../components/deck/Deck';
 
 const DiscardPile = ({cards}) => {
   return (
     <div>
-      {cards.map((card, index) => (
-        <Card {...card} key={card.id} index={index} revealed={false} />
-      ))}
+      <Deck deck={cards} />
     </div>
   )
 }
 
 DiscardPile.propTypes = {
-  cards: PropTypes.array.isRequired
+  cards: PropTypes.array.isRequired,
 }
 
 export default DiscardPile;
--- src/redux/reducers/discardPile.js
+++ src/redux/reducers/discardPile.js
@@ -1,11 +1,17 @@
 import { ADD_CARD_TO_DISCARD } from '../actions/discardPile';
+import update from 'react-addons-update';
 
 function discardReducer(state = [], action) {
   switch (action.type) {
     case ADD_CARD_TO_DISCARD:
+      const card = update(action, {
+        payload: {
+          cardlocation: {$set: 'DISCARD'}
+        }
+      }).payload
       return [
         ...state,
-        action.payload
+        card
       ]
     default:
       return state;
