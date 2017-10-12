2203cd24cb8d69d90207dc7aadc57b6bf16ae8f4
Dzianis Sipakou <d.sipakou@gmail.com>
2017-10-12 19:00:23 +0300

    Change popup location

--- src/components/deck/plotdeck/PlotDeck.js
+++ src/components/deck/plotdeck/PlotDeck.js
@@ -10,14 +10,8 @@ import { bindActionCreators } from 'redux';
 
 const customStyles = {
   content : {
-    top                   : '50%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
     height                : '40%',
     width                 : '90%',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)',
     display               : 'flex',
     flexFlow              : 'row wrap',
     alignItems            : 'center',
