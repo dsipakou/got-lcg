86bae59dfc8d2cbcaa25d0dc1349d099e3a7f096
flyermc <mc.flyer@gmail.com>
2017-10-31 23:40:18 +0300

    Make plot class from stateless

--- src/components/plot/Plot.js
+++ src/components/plot/Plot.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Deck from '../deck/Deck';
 import Card from '../card/Card';
+import PropTypes from 'prop-types';
 
-const Plot = ({ cards }) => {
-  return (
-    <div>
-      <h2>Plot in play</h2>
-      <Deck deck={cards} plot={true} revealed={true} />
-    </div>
-  )
+class Plot extends Component {
+  constructor() {
+    super();
+    this.doneTurn = this.doneTurn.bind(this);
+  }
+
+  componentDidMount() {
+    const { socket, gameflow } = this.props;
+    socket.on('opponent:done', () => {
+      gameflow.actions.opponentDone();
+    })
+  }
+
+  doneTurn() {
+
+  }
+
+  render() {
+    const { cards, gameflow } = this.props;
+    console.log(gameflow.payload.isOpponentDone)
+    return (
+      <div>
+        <h2>Plot in play</h2>
+        {gameflow.payload.isOpponentDone && <button onClick={this.doneTurn}>Next stage</button>}
+
+        <Deck deck={cards} plot={true} revealed={gameflow.payload.isOpponentDone} />
+      </div>
+    )
+  }
+}
+
+Plot.propTypes = {
+  cards: PropTypes.array.isRequired,
+  socket: PropTypes.object.isRequired,
+  gameflow: PropTypes.object.isRequired
 }
 
 export default Plot;
