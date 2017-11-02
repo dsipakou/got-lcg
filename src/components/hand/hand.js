0c89b462dceda69d2fb0bdf4028f29645f79488a
Dzianis Sipakou <d.sipakou@gmail.com>
2017-11-02 23:14:12 +0300

    Display card dublicates properly

--- src/components/hand/hand.js
+++ src/components/hand/hand.js
@@ -9,7 +9,7 @@ const Hand =({ cards, playCard }) => {
   return (
     <div className='hand'>
       { cards.map((card, index) => (
-        <DragableCard {...card} index={index} key={card.id} />
+        <DragableCard {...card} index={index} key={card.uid} />
       )) }
     </div>
   );
