f6066d8733e48693d065f98ce7c3afaeabc101f3
Dzianis Sipakou <d.sipakou@gmail.com>
2017-11-08 18:39:50 +0300

    Add challenge contols component

---
+++ src/components/challenges/ChallengeControls.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class ChallengeControls extends Component {
+  render() {
+    return (
+      <div>
+        <button>Military</button>
+        <button>Intigue</button>
+        <button>Power</button>
+      </div>
+    )
+  }
+}
+
+export default ChallengeControls;
