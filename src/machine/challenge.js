996d3fda881fdd5853eeb1ebed1f347d28d202ef
Dzianis Sipakou <d.sipakou@gmail.com>
2017-11-14 18:41:08 +0300

    Add new machine for challenge flow

---
+++ src/machine/challenge.js
@@ -0,0 +1,15 @@
+export default {
+  state: {
+    name: 'new challenge',
+    isPlayerDone: false,
+    isOpponentDone: false,
+    isYourTurn: false,
+    military: false,
+    intrigue: false,
+    power: false,
+    currentChallenge: '',
+  },
+  transitions: {
+    
+  }
+}
