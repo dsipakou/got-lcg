01d03c025a4765ad92c4a73d9c7904f9a1d9cc49
flyermc <mc.flyer@gmail.com>
2017-11-16 00:28:09 +0300

    Working on challenge controls

--- src/components/challenges/ChallengeControls.js
+++ src/components/challenges/ChallengeControls.js
@@ -18,39 +18,36 @@ class ChallengeControls extends Component {
       intrigue: false,
       power: false,
       hideControls: false,
-      yourTurn: this.props.isYourTurn,
-    }
+      yourTurn: false,
+      currentChallenge: '',
+    };
   }
 
   componentDidMount() {
     const { socket, gameflow } = this.props;
-
-    if (gameflow.state.isYourTurn) {
+    if (gameflow.payload.isYourTurn) {
       this.setState({ yourTurn: true });
     }
 
     socket.on('challenge:attack', () => {
-      this.setState({ hideControls: false })
+      this.setState({ hideControls: false });
     });
 
     socket.on('game:your turn', () => {
       gameflow.actions.yourTurn(true);
     });
-
     socket.on('challenge:set', (challenge) => {
+      this.setState({ currentChallenge: challenge });
       switch (challenge) {
-        case MILITARY: {
+        case MILITARY:
           this.setState({ military: true });
           break;
-        };
-        case INTRIGUE: {
-          this.setState({intrigue: true});
+        case INTRIGUE:
+          this.setState({ intrigue: true });
           break;
-        };
-        case POWER: {
-          this.setState({power: true});
+        case POWER:
+          this.setState({ power: true });
           break;
-        };
         default:
           break;
       }
@@ -58,27 +55,26 @@ class ChallengeControls extends Component {
   }
 
   chooseMilitary() {
-    const { gameflow, socket } = this.props;
-    this.setState({ military: true });
-    gameflow.challenges.setCurrentChallenge('MILITARY');
-    socket.emit('challenge:set', ('MILITARY'));
+    const { socket } = this.props;
+    this.setState({ military: true, currentChallenge: MILITARY });
+    socket.emit('challenge:set', (MILITARY));
   }
 
   chooseIntrigue() {
-    const { gameflow } = this.props;
-    this.setState({ intrigue: true });
-    gameflow.challenges.setCurrentChallenge('INTRIGUE');
+    const { socket } = this.props;
+    this.setState({ intrigue: true, currentChallenge: INTRIGUE });
+    socket.emit('challenge:set', (INTRIGUE));
   }
 
   choosePower() {
-    const { gameflow } = this.props;
-    this.setState({ power: true });
-    gameflow.challenges.setCurrentChallenge('POWER');
+    const { socket } = this.props;
+    this.setState({ power: true, currentChallenge: POWER });
+    socket.emit('challenge:set', (POWER));
   }
 
   attack() {
     const { socket, gameflow } = this.props;
-    this.setState({hideContorls: true});
+    this.setState({ hideContorls: true });
     socket.emit('challenge:attack');
     socket.emit('game:your turn');
     gameflow.actions.yourTurn(false);
@@ -87,12 +83,21 @@ class ChallengeControls extends Component {
   render() {
     const { gameflow } = this.props;
     const militaryButton = !this.state.military ? 'Military' : 'Done military';
+    if (this.state.yourTurn) {
+      return (
+        <div>
+          { this.state.currentChallenge !== '' && <span>Current: {this.state.currentChallenge}</span> }
+          { !(this.state.currentChallenge === '') && <button onClick={this.attack}>Attack</button>}
+          { this.state.currentChallenge === '' && !this.state.military && <button onClick={this.chooseMilitary}>{militaryButton}</button> }
+          { this.state.currentChallenge === '' && !this.state.intrigue && <button onClick={this.chooseIntrigue}>Intrigue</button> }
+          { this.state.currentChallenge === '' && !this.state.power && <button onClick={this.choosePower}>Power</button> }
+        </div>
+      );
+    }
     return (
       <div>
-        { !(gameflow.challenges.currentChallenge === '') && <button onClick={this.attack}>Attack</button>}
-        { gameflow.challenges.currentChallenge === '' && !this.state.military && <button onClick={this.chooseMilitary}>{militaryButton}</button> }
-        { gameflow.challenges.currentChallenge === '' && !this.state.intrigue && <button onClick={this.chooseIntrigue}>Intrigue</button> }
-        { gameflow.challenges.currentChallenge === '' && !this.state.power && <button onClick={this.choosePower}>Power</button> }
+        { this.state.currentChallenge !== '' && <span>Current: {this.state.currentChallenge}</span> }
+        <span>Wait for opponent complete</span>
       </div>
     );
   }
