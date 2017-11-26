dc0fe231eb2a24baef5a770db16882990c1c93cc
flyermc <mc.flyer@gmail.com>
2017-11-26 20:12:44 +0300

    Store machine state into redux state on each change

--- src/redux/actions/general/game.js
+++ src/redux/actions/general/game.js
@@ -1,7 +1,15 @@
 export const NEW_GAME = 'NEW_GAME';
+export const UPDATE_MACHINE = 'UPDATE_MACHINE';
 
 export const newGame = () => {
   return {
     type: NEW_GAME,
+  };
+};
+
+export const updateMachine = (payload) => {
+  return {
+    type: UPDATE_MACHINE,
+    payload: payload,
   };
 };
