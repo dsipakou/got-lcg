a08a6dfd4e88c9eedc6bea636bb68e3ff4c14d46
Dzianis Sipakou <d.sipakou@gmail.com>
2017-11-28 18:02:23 +0300

    Replace actions by dispatch function

--- src/containers/playerside/starthand/StartHand.js
+++ src/containers/playerside/starthand/StartHand.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import './StartHand.scss';
 import Card from '../../../components/card/Card';
 import MainDeck from '../../../components/deck/maindeck/MainDeck';
 import Messages from '../../../components/general/Messages';
 import { makeDeck, getStartHand, doMulligan } from '../../../redux/actions/player/deck';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import './StartHand.scss';
 
 class StartHand extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       canMulligan: true,
-    }
+    };
     this.doMulligan = this.doMulligan.bind(this);
     this.doneStage = this.doneStage.bind(this);
   }
@@ -25,13 +24,14 @@ class StartHand extends Component {
     });
     socket.on('game:plot', () => {
       gameflow.actions.gotoPlot();
-    })
+    });
   }
 
-  doMulligan(e) {
-    this.setState({canMulligan: false});
-    this.props.deckActions.doMulligan();
-    this.props.deckActions.getStartHand();
+  doMulligan() {
+    const { dispatch } = this.props;
+    this.setState({ canMulligan: false });
+    dispatch(doMulligan());
+    dispatch(getStartHand());
   }
 
   doneStage() {
@@ -45,57 +45,65 @@ class StartHand extends Component {
     }
   }
 
-  render () {
-    const { deck, hand, deckActions, gameflow, socket } = this.props
-    const initDeck = deckActions.makeDeck();
+  render() {
+    const {
+      dispatch,
+      deck,
+      hand,
+      gameflow,
+      socket,
+    } = this.props;
+    if (this.state.canMulligan) {
+      dispatch(makeDeck());
+    }
     return (
-      <div className='starthand-inner'>
-        { !gameflow.payload.isPlayerDone && <Messages text='Choose your start hand' /> }
-        { gameflow.payload.isPlayerDone && <Messages text='Wait for opponent done' /> }
-        <div className='starthand-content'>
-          <div className='starthand-cardlist'>
-          {
-            hand.map((card, index) => (
-              <Card {...card} index={index} key={card.uid} />
-            ))
-          }
+      <div className="starthand-inner">
+        { !gameflow.payload.isPlayerDone && <Messages text="Choose your start hand" /> }
+        { gameflow.payload.isPlayerDone && <Messages text="Wait for opponent done" /> }
+        <div className="starthand-content">
+          <div className="starthand-cardlist">
+            {
+              hand.map((card, index) => (
+                <Card {...card} index={index} key={card.uid} />
+              ))
+            }
           </div>
-          <div className='starthand-buttons'>
-            { !gameflow.payload.isPlayerDone && this.state.canMulligan && hand.length > 0 ? <button onClick={this.doMulligan}>Do mulligan</button> : null }
-            { !gameflow.payload.isPlayerDone && hand.length > 0 ? <button onClick={this.doneStage}>Done</button> : null }
+          <div className="starthand-buttons">
+            {
+              !gameflow.payload.isPlayerDone &&
+              this.state.canMulligan &&
+              hand.length > 0 ? <button onClick={this.doMulligan}>Do mulligan</button> : null
+            }
+            {
+              !gameflow.payload.isPlayerDone &&
+              hand.length > 0 ? <button onClick={this.doneStage}>Done</button> : null
+            }
           </div>
         </div>
-        <div className='starthand-footer'>
-          <MainDeck deck={initDeck} action={hand.length == 0 ? deckActions.getStartHand : ()=>{} } gameflow={gameflow} socket={socket} />
+        <div className="starthand-footer">
+          <MainDeck
+            deck={deck}
+            action={hand.length === 0 ? dispatch(getStartHand()) : () => {}}
+            gameflow={gameflow}
+            socket={socket}
+          />
         </div>
       </div>
-    )
+    );
   }
 }
 
 StartHand.propTypes = {
   socket: PropTypes.object.isRequired,
+  dispatch: PropTypes.func.isRequired,
   deck: PropTypes.array.isRequired,
   hand: PropTypes.array.isRequired,
-  deckActions: PropTypes.shape({
-    makeDeck: PropTypes.func.isRequired,
-    getStartHand: PropTypes.func.isRequired,
-    doMulligan: PropTypes.func.isRequired,
-  }),
   gameflow: PropTypes.object.isRequired,
 }
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   deck: state.player.deckReducer,
   hand: state.player.handReducer,
-})
+});
 
-const mapDispatchToProps = (dispatch) => ({
-  deckActions: bindActionCreators({
-    makeDeck,
-    getStartHand,
-    doMulligan
-  }, dispatch),
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(StartHand)
+export default connect(mapStateToProps)(StartHand);
