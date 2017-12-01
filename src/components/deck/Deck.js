34742a5b732f81233fc1afb06ce72fed9c99fe78
Dzianis Sipakou <d.sipakou@gmail.com>
2017-12-01 17:20:06 +0300

    Refactoring

--- src/components/deck/Deck.js
+++ src/components/deck/Deck.js
@@ -1,40 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import './Deck.scss';
 
-const Deck = ({ deck, text, plot, revealed }) => {
+const Deck = ({
+  deck,
+  text,
+  plot,
+  revealed,
+}) => {
+  const emptyDeck = deck.length === 0;
 
-  const emptyDeck = deck.length == 0;
-
-  let deckClass = classNames({
+  const deckClass = classNames({
     'deck-inner': true,
     'empty-deck': emptyDeck,
-    'plot': plot,
-    'revealed': revealed,
+    plot,
+    revealed,
   });
 
-  const topCard = !emptyDeck ? deck[deck.length - 1] : 0
+  const topCard = !emptyDeck ? deck[deck.length - 1] : 0;
 
   return (
     <div className={deckClass}>
-      { revealed && !emptyDeck && <img src={topCard.image_url} />}
+      { revealed && !emptyDeck && <img src={topCard.image_url} alt="Card" />}
       { (!revealed || emptyDeck) && <span>{text}</span>}
-      <div className='deck-spinner'></div>
+      <div className="deck-spinner" />
     </div>
-  )
-}
+  );
+};
 
 Deck.propTypes = {
   deck: PropTypes.array.isRequired,
   text: PropTypes.string,
   plot: PropTypes.bool,
   revealed: PropTypes.bool,
-}
+};
 
 Deck.defaultTypes = {
   plot: false,
   revealed: false,
-}
+};
 
 export default Deck;
