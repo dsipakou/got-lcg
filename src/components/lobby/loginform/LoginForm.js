e14919c915efaf73ce10d967b6ccac72068606af
flyermc <mc.flyer@gmail.com>
2017-12-02 22:58:34 +0300

    Refactoring Login form

--- src/components/lobby/loginform/LoginForm.js
+++ src/components/lobby/loginform/LoginForm.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const LoginForm = ({login}) => {
+const LoginForm = ({
+  login,
+}) => {
   return (
     <div>
-      <input id='username' />
-      <button id='login_btn' onClick={login}>Login</button>
+      <input id="username" />
+      <button id="login_btn" onClick={login}>Login</button>
     </div>
-  )
-}
+  );
+};
 
 LoginForm.propTypes = {
-  login: PropTypes.func
-}
+  login: PropTypes.func.isRequired,
+};
 
 export default LoginForm;
