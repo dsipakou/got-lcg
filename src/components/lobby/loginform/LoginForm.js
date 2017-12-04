76b3d09f7f4fbf45dd4401f46e89034ff905cf8b
Dzianis Sipakou <d.sipakou@gmail.com>
2017-12-04 19:31:36 +0300

    Handle sign in

--- src/components/lobby/loginform/LoginForm.js
+++ src/components/lobby/loginform/LoginForm.js
@@ -1,16 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Cookies from 'universal-cookie';
 
-const LoginForm = ({
-  login,
-}) => {
-  return (
-    <div>
-      <input id="username" />
-      <button id="login_btn" onClick={login}>Login</button>
-    </div>
-  );
-};
+const cookies = new Cookies();
+
+class LoginForm extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      password: '',
+    };
+    this.usernameChange = this.usernameChange.bind(this);
+    this.passwordChange = this.passwordChange.bind(this);
+    this.doLogin = this.doLogin.bind(this);
+  }
+
+  usernameChange(event) {
+    this.setState({
+      username: event.target.value,
+    });
+  }
+
+  passwordChange(event) {
+    this.setState({
+      password: event.target.value,
+    });
+  }
+
+  doLogin() {
+    fetch('http://localhost:8000/auth/', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password,
+      }),
+    }).then((response) => {
+      if (response.ok) {
+        response.json().then((json) => {
+          cookies.set('token', json.token);
+        });
+      }
+    }).catch((error) => { throw error; });
+  }
+
+  render() {
+    return (
+      <div>
+        <input id="username" onChange={this.usernameChange} />
+        <input id="password" onChange={this.passwordChange} />
+        <button id="login_btn" onClick={this.doLogin}>Login</button>
+      </div>
+    );
+  }
+}
 
 LoginForm.propTypes = {
   login: PropTypes.func.isRequired,
