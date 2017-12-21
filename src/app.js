99461e8a4bf04df626bb3c341d429b675ade97a0
Dzianis Sipakou <d.sipakou@gmail.com>
2017-12-21 19:02:41 +0300

    Add PersistGate

--- src/app.js
+++ src/app.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/es/integration/react'
 import { browserHistory, BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import Board from './containers/board/Board';
 import Lobby from './containers/lobby/Lobby';
 import Layout from './components/Layout';
-import store from './redux/store';
+import configureStore from './redux/store';
 import './style/base.scss';
 
 const socket = require('socket.io-client')('http://localhost:3000');
+
+const { store, persistor } = configureStore();
 
 const connectSocket = (component) => {
   return props => React.createElement(component, { socket, ...props });
@@ -20,7 +23,12 @@ render(
     <BrowserRouter>
       <Layout>
         <Switch>
+          <PersistGate
+            loading={<div>Hello loading</div>}
+            persistor={persistor}
+          >
           <Route exact path="/" component={connectSocket(Board)} />
+          </PersistGate>
           <Route path="/lobby" component={connectSocket(Lobby)} />
         </Switch>
       </Layout>
