556622d92d541cfa1bb16ddd0ff81e337360f7d7
Dzianis Sipakou <d.sipakou@gmail.com>
2017-12-29 11:12:08 +0300

    Fixing errors

--- src/components/deck/discardPile/DiscardPile.js
+++ src/components/deck/discardPile/DiscardPile.js
@@ -2,16 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Deck from '../Deck';
 
-const DiscardPile = (cards) = {
+const DiscardPile = (cards) => {
   return (
     <div>
       <Deck deck={cards} text="Discard Pile" />
     </div>
-  )
-}
+  );
+};
 
 DiscardPile.propTypes = {
   cards: PropTypes.array.isRequired,
-}
+};
 
 export default DiscardPile;
