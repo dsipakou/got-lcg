d48fd64c2d73e772902f5fba1595811cbfd842aa
Denis Sipakov <denis.sipakov@pandadoc.com>
2018-06-27 12:39:34 +0300

    Refactor

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
-import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/es/integration/react'
-import { browserHistory, BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import BoardContainer from './containers/board/BoardContainer';
 import Lobby from './containers/lobby/Lobby';
 import Layout from './components/Layout';
@@ -25,13 +24,13 @@ render(
         <Switch>
           <PersistGate
             loading={<div>Hello loading</div>}
-            persistor={persistor}
-          >
+            persistor={persistor}>
             <Route exact path="/" component={connectSocket(BoardContainer)} />
           </PersistGate>
           <Route path="/lobby" component={connectSocket(Lobby)} />
+          <Route path='/test' component={Lobby} />
         </Switch>
-      </Layout>
+    </Layout>
     </BrowserRouter>
   </Provider>,
   document.getElementById('app'),
